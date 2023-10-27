57458ea99c8a4a0885a114d2ef789df013424a09
TeddyFrye <tfrye1227@gmail.com>
2023-10-27 11:35:51 -0500

    Adds code to track score

---
+++ src/components/Scoreboard.jsx
@@ -0,0 +1,10 @@
+function Scoreboard({ currentScore, bestScore }) {
+  return (
+    <div className="scoreboard">
+      <div>Current Score: {currentScore}</div>
+      <div>Best Score: {bestScore}</div>
+    </div>
+  );
+}
+
+export default Scoreboard;
