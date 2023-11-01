0d3166991d1df13bbe1e9fce962deb101348c6bb
TeddyFrye <tfrye1227@gmail.com>
2023-11-01 11:44:23 -0500

    Adds professor png, moves index style sheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import Gameboard from "./components/GameBoard.jsx";
 import Scoreboard from "./components/Scoreboard.jsx";
+import "./styles/App.css";
 import "./styles/Gameboard.css";
+import "./styles/Card.css";
+import "./styles/Scoreboard.css";
+import "./styles/index.css";
 
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
@@ -16,8 +20,12 @@ function App() {
 
   return (
     <>
-      <Scoreboard currentScore={currentScore} bestScore={bestScore} />
-      <Gameboard updateScore={updateScore} />
+      <div className="scoreboard-container">
+        <Scoreboard currentScore={currentScore} bestScore={bestScore} />
+      </div>
+      <div className="gameboard-container">
+        <Gameboard updateScore={updateScore} />
+      </div>
     </>
   );
 }
